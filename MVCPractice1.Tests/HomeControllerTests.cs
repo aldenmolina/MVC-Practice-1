6a667072447fc3cba522200fa1a05784ef504dae
Alden <aj.molina092@gmail.com>
2019-02-18 15:43:23 -0500

    Created controllers, views, and test

--- MVCPractice1.Tests/HomeControllerTests.cs
+++ MVCPractice1.Tests/HomeControllerTests.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using MVCPractice1.Controllers;
 using Xunit;
 
 namespace MVCPractice1.Tests
 {
-    class HomeControllerTests
+    public class HomeControllerTests
     {
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            var underTest = new HomeController();
+
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
     }
 }
